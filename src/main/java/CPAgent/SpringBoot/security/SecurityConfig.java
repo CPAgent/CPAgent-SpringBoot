2d3ee1fcbaa177209dc4751750d79476db0c2368
Juwon Hwang <juwonhwang0411@gmail.com>
2025-10-16 17:59:34 +0900

    SecurityConfig 클래스에서 사용하지 않는 AuthenticationManager 임포트 제거

--- src/main/java/CPAgent/SpringBoot/security/SecurityConfig.java
+++ src/main/java/CPAgent/SpringBoot/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package CPAgent.SpringBoot.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
